50d587a642c598901ba3eb342360fe4029c02b53
Szymon <joceksz@wp.pl>
2023-05-18 11:25:03 +0200

    fields values

---
+++ src/lib/initialFormikValues.ts
@@ -0,0 +1,13 @@
+import { DishesFields } from "../types";
+
+const initialFormikValues: DishesFields = {
+  diameter: 0,
+  name: "",
+  no_of_slices: 0,
+  preparation_time: "",
+  slices_of_bread: 0,
+  spiciness_scale: 0,
+  type: "",
+};
+
+export default initialFormikValues;
