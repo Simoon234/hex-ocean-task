0e199daa21162ce09266847a05dec81d620b9c5c
Szymon <joceksz@wp.pl>
2023-05-18 12:24:49 +0200

    interface for errors - while testing in postman I had realized that response errors are returning object with messages that are a type of string[];

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -9,3 +9,13 @@ export interface DishesFields {
   spiciness_scale?: number | null;
   slices_of_bread?: number | null;
 }
+
+export interface ErrorRes {
+  slices_of_bread: string[] | null;
+  name: string[] | null;
+  type: string[] | null;
+  preparation_time: string[] | null;
+  no_of_slices: string[] | null;
+  diameter: string[] | null;
+  spiciness_scale: string[] | null;
+}
