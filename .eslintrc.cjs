3912a19889f0965feb1b5d5c58ce3c114c335193
Szymon <joceksz@wp.pl>
2023-05-18 14:45:00 +0200

    disable one rule related with props

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
-    'import/no-extraneous-dependencies': 'off'
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 }
