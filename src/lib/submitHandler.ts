f81c80e02223193f13fd7adb319cdaf7f252789d
Szymon <joceksz@wp.pl>
2023-05-18 15:31:41 +0200

    return values

--- src/lib/submitHandler.ts
+++ src/lib/submitHandler.ts
@@ -1,30 +1,31 @@
 import { DishType, Submit } from "../types";
 
-const submitHelper = ({ mutate, val }: Submit) => {
+const submitHandler = ({ mutate, val }: Submit) => {
   const requiredValues = {
     name: val.name,
     preparation_time: val.preparation_time,
     type: val.type,
   };
   if (val.type === DishType.pizza) {
-    mutate({
+    return mutate({
       ...requiredValues,
       no_of_slices: val.no_of_slices,
       diameter: val.diameter,
     });
   }
   if (val.type === DishType.soup) {
-    mutate({
+    return mutate({
       ...requiredValues,
       spiciness_scale: val.spiciness_scale,
     });
   }
   if (val.type === DishType.sandwich) {
-    mutate({
+    return mutate({
       ...requiredValues,
       slices_of_bread: val.slices_of_bread,
     });
   }
+  return false;
 };
 
-export default submitHelper;
+export default submitHandler;
